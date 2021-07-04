f845f43076263753e354c9c12310970bb6461832
Tom Bartle <1022049@student.sae.edu.au>
2021-07-04 17:28:17 +1000

    Created mage placeholder animations

---
+++ Assets/Scripts/AbilityBase.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityBase : ScriptableObject
+{
+    public new string name;
+    public float cooldownTime;
+    public float activeTime;
+}
---
+++ Assets/Scripts/ProjectileAbility.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Projectile Ability", menuName = "Ability")]
+public class ProjectileAbility : AbilityBase
+{
+    public GameObject projectile;
+}
